a98d135fe7218f21f0496ed5dffb4aaa324e62d9
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-01-22 18:51:36 +0000

    Add files via upload

---
+++ puzzleSpec.js
@@ -0,0 +1,12 @@
+let punterPuzzleSpec = {
+	number: 1,
+	solveBy: "28 Jan",
+	wordSpec: [undefined, "LEANNA", "RENATA", "ROWENA", "ROSALEE", "RAMONA"],
+	hintSpec: ["R", [4, 1]],
+	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
+	featuredWordBlurb:
+		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
+		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+};
+
+

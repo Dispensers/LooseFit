07f41e4d7a29e9b51f9bed4a16c5d9a4e7b3bef1
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-02-02 18:35:14 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,12 +1,99 @@
+
+let punterPuzzleSpec = {
+	number: 21,
+	solveBy: "8 Feb",
+	wordSpec: [undefined, "ANDREA", "ARLENE", "JANICE", "SERENA", "ELAINE"],
+	hintSpec: ["N", [2, 5]],
+	solutionSpec: [undefined, [3, 1], [4, 2], [1, 1], [5, 1], [2, 1]],
+	featuredWordBlurb:
+		"It seems the first JANICE was born in 1899. " +
+		"She was the eponymous heroine of the novel JANICE Meredith by the American author Paul Leicester Ford."
+}
+
+
+
+/*
 let punterPuzzleSpec = {
 	number: 1,
 	solveBy: "28 Jan",
-	wordSpec: [undefined, "LEANNA", "RENATA", "ROWENA", "ROSALEE", "RAMONA"],
+	wordSpec: [undefined, "DEADEN", "DENUDE", "DUNDEE", "LAUDED", "ADELE"],
 	hintSpec: ["R", [4, 1]],
 	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
 	featuredWordBlurb:
 		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
 		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
 };
-
+*/
+/*
+let punterPuzzleSpec = {
+	number: 1,
+	solveBy: "28 Jan",
+	wordSpec: [undefined, "EGGNOG", "GEORGE", "GORGON", "ORANGE", "OREGON"],
+	hintSpec: ["R", [4, 1]],
+	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
+	featuredWordBlurb:
+		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
+		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+};
+*?
+/*
+let punterPuzzleSpec = {
+	number: 1,
+	solveBy: "28 Jan",
+	wordSpec: [undefined, "LUPINE", "NELLIE", "SENILE", "SPELLS", "UNSEEN"],
+	hintSpec: ["R", [4, 1]],
+	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
+	featuredWordBlurb:
+		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
+		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 1,
+	solveBy: "28 Jan",
+	wordSpec: [undefined, "COCOON", "CORONA", "RACOON", "ROCOCO", "CACAO"],
+	hintSpec: ["R", [4, 1]],
+	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
+	featuredWordBlurb:
+		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
+		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 1,
+	solveBy: "28 Jan",
+	wordSpec: [undefined, "CAMPED", "DECAMP", "DEEMED", "MADCAP", "MAPPED"],
+	hintSpec: ["R", [4, 1]],
+	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
+	featuredWordBlurb:
+		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
+		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 1,
+	solveBy: "28 Jan",
+	wordSpec: [undefined, "EMERGE", "GREENE", "MEAGRE", "RENEGE", "GERMANE"],
+	hintSpec: ["R", [4, 1]],
+	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
+	featuredWordBlurb:
+		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
+		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 1,
+	solveBy: "28 Jan",
+	wordSpec: [undefined, "BIKINI", "INKING", "MISFIT", "NITWIT", "KINGPIN"],
+	hintSpec: ["R", [4, 1]],
+	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
+	featuredWordBlurb:
+		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
+		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+};
+*/
 

3144b2bfc3343c631fbebc3c767fa1cb5b9bec28
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-03-08 21:58:06 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,176 +1,107 @@
 /*
 let punterPuzzleSpec = {
-	number: 21,
-	solveBy: "8 Feb",
-	wordSpec: [undefined, "ANDREA", "ARLENE", "JANICE", "SERENA", "ELAINE"],
-	hintSpec: ["N", [2, 5]],
-	solutionSpec: [undefined, [3, 1], [4, 2], [1, 1], [5, 1], [2, 1]],
-	featuredWordBlurb:
-		"It seems the first JANICE was born in 1899. " +
-		"She was the eponymous heroine of the novel JANICE Meredith by the American author Paul Leicester Ford."
-}
+	number: 12,
+	solveBy: "6 Feb",
+	dispenserSpec: [undefined, "26", "4*", "/05"],
+	targetSpec: "200",
+	hintSpec: {numDots: 3, symbol:"0", isHere: false},
+	solutionExpression: "50/6*24",
+	solutionDispenseSequence: [3, 3, 3, 1, 2, 1, 2]
+};
 */
 /*
 let punterPuzzleSpec = {
 	number: 15,
-	solveBy: "15 Feb",
-	wordSpec: [undefined, "HOBNOB", "HOODOO", "AUROCHS", "BOOHOO", "HURRAH"],
-	hintSpec: ["O", [5, 3]],
-	solutionSpec: [undefined, [3, 2], [5, 2], [4, 1], [2, 1], [1, 1]],
-	featuredWordBlurb:
-		"AUROCHS is a cattle species. Now extinct. " +
-		"Its massive horns - up to 80cm in length - ensured it was heavily hunted. " +
-		"Considered to be the wild ancestor of modern domestic cattle. AUROCHS is both the singular and the plural."
-}
-*/
-/*
-let punterPuzzleSpec = {
-	number: 16,
-	solveBy: "22 Feb",
-	wordSpec: [undefined, "ROTTER", "TORPOR", "OPORTO", "RESORT", "PRESTO"],
-	hintSpec: ["T", [3, 6]],
-	solutionSpec: [undefined, [4, 2], [2, 2], [5, 2], [3, 1], [1, 1]],
-	featuredWordBlurb:
-		"OPORTO or Porto is Portugal's second city. " +
-		"It lies in the north of the country on the Douro River estuary. Port wine is produced in the Douro valley."
-}
-*/
-/*
-let punterPuzzleSpec = {
-	number: 17,
-	solveBy: "29 Feb",
-	wordSpec: [undefined, "QUEUES", "QUICHE", "QUEBEC", "SEQUEL", "UNIQUE"],
-	hintSpec: ["U", [1, 2]],
-	solutionSpec: [undefined, [4, 1], [5, 1], [2, 1], [3, 2], [1, 2]],
-	featuredWordBlurb:
-		"In 1995, QUEBEC very nearly set out on the path to independence. "+
-		"A referendum asking whether or not the province should separate from Canada resulted in a 49.42%-50.58% Yes-No verdict. " +
-		"Just 54,288 votes in it! Turnout was 93.52%!"
-}
-*/
-
-let punterPuzzleSpec = {
-	number: 18,
-	solveBy: "7 Mar",
-	wordSpec: [undefined, "KNEELER", "NICKEL", "KENKEN", "ENCORE", "KEENER"],
-	hintSpec: ["E", [5, 2]],
-	solutionSpec: [undefined, [4, 1], [1, 2], [2, 1], [3, 2], [5, 1]],
-	featuredWordBlurb:
-		"KENKEN is a Sudoku-like puzzle. Invented in Japan in 2004. " +
-		"The puzzle has an arithmetic element as well as a logic element. This duality is making it a classroom hit!"
-}
-
-/*
-let punterPuzzleSpec = {
-	number: 19,
-	solveBy: "7 Mar",
-	wordSpec: [undefined, "NITRIC", "INGRID", "RISHI", "RIMINI", "CRITIC"],
-	hintSpec: ["N", [2, 5]],
-	solutionSpec: [undefined, [4, 1], [1, 2], [2, 2], [5, 1], [3, 2]],
-	featuredWordBlurb:
-		"RISHI Sunak's occupancy of the Number 10 throne may be a little precarious, but he'll always be able to sit comfortably in his Richmond seat. " +
-		"The Yorkshire constituency has been held by the Tories for over 100 years."
-}
-*/
-/*
-let punterPuzzleSpec = {
-	number: 20,
-	solveBy: "14 Mar",
-	wordSpec: [undefined, "CAESAR", "SENATE", "SENECA", "SCENES", "SEANCE"],
-	hintSpec: ["N", [2, 5]],
-	solutionSpec: [undefined, [5, 2], [2, 1], [3, 1], [4, 2], [1, 2]],
-	featuredWordBlurb:
-		"SENECA was twice ordered to kill himself! When Caligula was offended by his success as an orator! " +
-		"(He survived because he was very ill and deemed to be as good as dead.) And when Nero decided he was part of an assassination plot."
-}
-*/
-/*
-let punterPuzzleSpec = {
-	number: 22,
-	solveBy: "?? Apr",
-	wordSpec: [undefined, "MISFIT", "INKING", "BIKINI", "NITWIT", "KINGPIN"],
-	hintSpec: ["R", [4, 1]],
-	solutionSpec: [undefined, [2, 1], [1, 2], [4, 1], [5, 2], [3, 1]],
-	featuredWordBlurb:
-		"Frenchman Louis Rèard was an automobile engineer ... and a clothing designer! " +
-		"In 1946, he launched the BIKINI. He named it after the BIKINI Atoll (in the Marshall Islands), where a nuclear bomb had recently been tested."
+	solveBy: "13 Feb",
+	dispenserSpec: [undefined, "-1", "4*1", "71"],
+	targetSpec: "147",
+	hintSpec: {numDots: 3, symbol:"7", isHere: false},
+	solutionExpression: "11*14-7",
+	solutionDispenseSequence: [1, 2, 2, 3, 2, 1, 3]
 };
 */
 
-/* ================================================================================================================================================================ */
-
-
+let punterPuzzleSpec = {
+	number: 13,
+	solveBy: "20 Feb",
+	dispenserSpec: [undefined, "Nde", "ud", "du", "eS"],
+	mapSpec: [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+              [1, 2, 0, 1, 0, 0, 0, 1, 0, 1],				
+	          [1, 0, 0, 0, 0, 0, 1, 0, 9, 1],
+		      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		     ],
+	hintSpec: {coord: [3, 3], direction:"u"},
+	solutionSpec: [1, 1, 3, 1, 2, 4, 2, 3, 4]
+};
 
 /*
 let punterPuzzleSpec = {
 	number: 23,
-	solveBy: "28 Jan",
-	wordSpec: [undefined, "EMERGE", "GREENE", "MEAGRE", "RENEGE", "GERMANE"],
-	hintSpec: ["R", [4, 1]],
-	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
-	featuredWordBlurb:
-		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
-		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+	solveBy: "27 Feb",
+	dispenserSpec: [undefined, "13", "5", "79", "-", "*"],
+	targetSpec: "318",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "9*37-15",
+	solutionDispenseSequence: [3, 5, 1, 3, 4, 1, 2]
 };
 */
 /*
 let punterPuzzleSpec = {
-	number: 24,
-	solveBy: "28 Jan",
-	wordSpec: [undefined, "CAMPED", "DECAMP", "DEEMED", "MADCAP", "MAPPED"],
-	hintSpec: ["R", [4, 1]],
-	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
-	featuredWordBlurb:
-		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
-		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+	number: 17,
+	solveBy: "5 Mar",
+	dispenserSpec: [undefined, "20", "/", "+8", "46"],
+	targetSpec: "4.15",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "86/40+2",
+	solutionDispenseSequence: [3, 4, 2, 4, 1, 3, 1]
 };
 */
 /*
 let punterPuzzleSpec = {
-	number: 25,
-	solveBy: "28 Jan",
-	wordSpec: [undefined, "COCOON", "CORONA", "RACOON", "ROCOCO", "CACAO"],
-	hintSpec: ["R", [4, 1]],
-	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
-	featuredWordBlurb:
-		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
-		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+	number: 14,
+	solveBy: "12 Mar",
+	dispenserSpec: [undefined, "/61", "7", "91-8"],
+	targetSpec: "53",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "81-196/7",
+	solutionDispenseSequence: [3, 1, 3, 3, 3, 1, 1, 2]
 };
 */
 /*
 let punterPuzzleSpec = {
-	number: 26,
-	solveBy: "28 Jan",
-	wordSpec: [undefined, "LUPINE", "NELLIE", "SENILE", "SPELLS", "UNSEEN"],
-	hintSpec: ["R", [4, 1]],
-	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
-	featuredWordBlurb:
-		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
-		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+	number: 22,
+	solveBy: "19 Mar",
+	dispenserSpec: [undefined, "1", "2+", "3", "4-", "5"],
+	targetSpec: "-28",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "1-53+24",
+	solutionDispenseSequence: [1, 4, 5, 3, 2, 2, 4]
 };
 */
 /*
 let punterPuzzleSpec = {
-	number: 27,
-	solveBy: "28 Jan",
-	wordSpec: [undefined, "EGGNOG", "GEORGE", "GORGON", "ORANGE", "OREGON"],
-	hintSpec: ["R", [4, 1]],
-	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
-	featuredWordBlurb:
-		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
-		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
-};
-*/
-/*
-let punterPuzzleSpec = {
-	number: 28,
-	solveBy: "28 Jan",
-	wordSpec: [undefined, "DEADEN", "DENUDE", "DUNDEE", "LAUDED", "ADELE"],
-	hintSpec: ["R", [4, 1]],
-	solutionSpec: [undefined, [2, 1], [5, 1], [4, 1], [1, 1], [3, 1]],
-	featuredWordBlurb:
-		"In English, it's Switzerland; in French, <strong>SUISSE</strong>; in German, Schweiz; in Italian, Svizzera.\
-		Officially, it's Confoederatio Helvetica (CH) - the Latin name."
+	number: 16,
+	solveBy: "?? Apr",
+	dispenserSpec: [undefined, "5-1", "159", "-19"],
+	targetSpec: "-37",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "915-951-1",
+	solutionDispenseSequence: [2, 1, 2, 1, 3, 1, 3, 3, 2]
 };
 */
 
+/* ================================================================= */
+
+
+
+/*
+let punterPuzzleSpec = {
+	number: 99,
+	solveBy: "30 Jan",
+	dispenserSpec: [undefined, "/9", "6-", "38", "0", "1"],
+	targetSpec: "28",
+	solutionExpression: "90-186/3",
+	solutionDispenseSequence: [1, 4, 2, 5, 3, 2, 1, 3]
+};
+*/
